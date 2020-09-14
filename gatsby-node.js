bc97f7544a0577b40f062a1f129010914f2dca3d
althea <altheaserena.yi@gmail.com>
2020-09-13 20:14:01 -0400

    feat/ creates lunr search index in gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,67 @@ const path = require(`path`)
 const { postsPerPage } = require(`./src/utils/siteConfig`)
 const { paginate } = require(`gatsby-awesome-pagination`)
 
+const { GraphQLJSONObject } = require(`graphql-type-json`)
+const striptags = require(`striptags`)
+const lunr = require(`lunr`)
+
+exports.createResolvers = ({ cache, createResolvers }) => {
+    createResolvers({
+        Query: {
+            LunrIndex: {
+                type: GraphQLJSONObject,
+                resolve: (source, args, context, info) => {
+                    const blogNodes = context.nodeModel.getAllNodes({
+                        type: `MarkdownRemark`,
+                    })
+                    const type = info.schema.getType(`MarkdownRemark`)
+                    return createIndex(blogNodes, type, cache)
+                },
+            },
+        },
+    })
+}
+
+const createIndex = async (blogNodes, type, cache) => {
+    const cacheKey = `IndexLunr`
+    const cached = await cache.get(cacheKey)
+    if (cached) {
+        return cached
+    }
+    const documents = []
+    const store = {}
+    // Iterate over all posts
+    for (const node of blogNodes) {
+        const { slug } = node.fields
+        const title = node.frontmatter.title
+        const [html, excerpt] = await Promise.all([
+            type.getFields().html.resolve(node),
+            type.getFields().excerpt.resolve(node, { pruneLength: 40 }),
+        ])
+        // Once html is resolved, add a slug-title-content object to the documents array
+        documents.push({
+            slug: node.fields.slug,
+            title: node.frontmatter.title,
+            content: striptags(html),
+        })
+        store[slug] = {
+            title,
+            excerpt,
+        }
+    }
+    const index = lunr(function () {
+        this.ref(`slug`)
+        this.field(`title`)
+        this.field(`content`)
+        for (const doc of documents) {
+            this.add(doc)
+        }
+    })
+    const json = { index: index.toJSON(), store }
+    await cache.set(cacheKey, json)
+    return json
+}
+
 /**
  * Here is the place where Gatsby creates the URLs for all the
  * posts, tags, pages and authors that we fetched from the Ghost site.
